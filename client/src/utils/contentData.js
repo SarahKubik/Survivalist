9f627e63a19bd9b4769e357ed436347f6d978615
Adam And <andadam24@gmail.com>
2021-09-13 19:21:37 -0400

    updated links on contentData page to wikipedia

--- client/src/utils/contentData.js
+++ client/src/utils/contentData.js
@@ -1,25 +1,25 @@
 const contentData = [
   {
     title: "Hammer",
-    link: "https://auth0.com/docs/connections",
+    link: "https://en.wikipedia.org/wiki/Hammer",
     description:
       "This hammer is good! You can use it to nail items or even as a defensive weapon"
   },
   {
     title: "Water",
-    link: "https://auth0.com/docs/multifactor-authentication",
+    link: "https://en.wikipedia.org/wiki/Water",
     description:
       "Water is important to staying alive. Having safe drinking water can be used to keep you hydrated, used to cook food, and cleaning items."
   },
   {
     title: "Shelter",
-    link: "https://auth0.com/docs/anomaly-detection",
+    link: "https://en.wikipedia.org/wiki/Shelter",
     description:
       "Shelter is crucial to survivalism. You need proper shelter in order to keep yourself same from unknowns. "
   },
   {
     title: "Rope",
-    link: "https://auth0.com/docs/rules",
+    link: "https://en.wikipedia.org/wiki/Rope",
     description:
       "Rope can be a good item to have to help store items and build."
   }
