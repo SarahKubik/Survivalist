b399cd7adfa7db45f47f111f2fba488ef47eb0e8
CadenWit <cadenwitbeck@gmail.com>
2021-09-14 22:39:51 -0400

    uhaha

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,20 +1,67 @@
 const db = require("./connection");
-const { User } = require("../models");
+const { User, Item } = require("../models");
 
 db.once("open", async () => {
-  await User.create({
-    Name: "Pamela",
-    email: "pamela@testmail.com",
-    password: "password12345",
-  });
+  await User.deleteMany({});
 
-  await User.create({
-    Name: "Holt",
-    email: "eholt@testmail.com",
-    password: "password12345",
-  });
+  const users = await User.insertMany([
+    {
+      name: "A",
+      email: "A@gmail.com",
+      password: "admin",
+      wishlist: [],
+    },
+    {
+      name: "B",
+      email: "B@gmail.com",
+      password: "admin",
+      wishlist: [],
+    },
+    {
+      name: "C",
+      email: "C@gmail.com",
+      password: "admin",
+      wishlist: [],
+    },
+    {
+      name: "D",
+      email: "D@gmail.com",
+      password: "admin",
+      wishlist: [],
+    },
+  ]);
 
   console.log("users seeded");
 
+  await Item.deleteMany();
+
+  console.log("items seeded");
+
+  const item = await Item.insertMany([
+    {
+      name: "Item1",
+      description: "SOmethin Item1",
+      image: "something",
+      price: "100",
+    },
+    {
+      name: "Item2",
+      description: "SOmethin Item2",
+      image: "something",
+      price: "200",
+    },
+    {
+      name: "Item3",
+      description: "SOmethin Item3",
+      image: "something",
+      price: "300",
+    },
+    {
+      name: "Item4",
+      description: "SOmethin Item4",
+      image: "something",
+      price: "400",
+    },
+  ]);
   process.exit();
 });
