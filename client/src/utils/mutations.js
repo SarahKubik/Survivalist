c451bd549117fb10840ca304f238773dac2c29e8
Drew Selden <seldend1@gmail.com>
2021-09-16 01:35:57 -0400

    mutations.js with login mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }`
+  
+export const ADD_USER = gql`
+    mutation addUser($name: String!, $email: String!, $password: String!) {
+        addUser(name: $name, email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+export const ADD_ITEM = gql`
+    mutation addItem($name: String!, $description: String!, $image: String!, $price: Float!) {
+        addItem(name: $name, description: $description, image: $image, price: $price) {
+            _id
+            name
+            description
+            image
+            price
+        }
+    }
+`;
+
