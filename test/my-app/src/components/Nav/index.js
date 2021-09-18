bf5b980c1f0e760a454fac75be4374c5db445578
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-18 00:11:45 -0400

    update hammer

--- test/my-app/src/components/Nav/index.js
+++ test/my-app/src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import { Icon } from '@iconify/react';
 
 function Nav() {
 
@@ -43,8 +44,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+        <Icon icon="noto:hammer-and-pick" />
+          -Survivalists
         </Link>
       </h1>
 
