0576f6fb1bf9ee0a360bf8abb4f5d451d47ea31a
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-18 04:02:46 -0400

    assets

---
+++ test/my-app/src/utils/contentData.js
@@ -0,0 +1,37 @@
+const contentData = [
+  {
+    title: "Tools",
+    image: ".assets/hammer.jpg",
+    link: "https://en.wikipedia.org/wiki/Hammer",
+    description:
+      "Tools such as hammers, knives, and axes can be helpful to establishing a shelter as well as for defense purposes. See the link above to learn more about tools and how they can be an asset.",
+    price: 5.99
+
+  },
+  {
+    title: "Water & Filtration",
+    image: ".assets/boxed water.jpg",
+    link: "https://en.wikipedia.org/wiki/Water",
+    description:
+      "Water, more importantly, properly filtrated water is an essential component to survivalism. To learn more, click the link above.",
+    price: 10.00
+  },
+  {
+    title: "Shelter",
+    image: ".assets/shelter.jpg",
+    link: "https://en.wikipedia.org/wiki/Shelter",
+    description:
+      "In order to survive in all weather conditions, you will need to have more than a basic understanding of how to assemble a shelter. Click the link above to learn more about various types of shelters and how they can help you survive the dangerous unknown.",
+    price: 100.00
+  },
+  {
+    title: "Supplies",
+    image: '.assets/shelter.jpg',
+    link: "https://en.wikipedia.org/wiki/Rope",
+    description:
+      "There are various supplies from rope to compasses to dry packs that we recommend looking into before starting your Survivalist journey. See link above to learn more.",
+     price: 10.00
+  }
+];
+
+export default contentData;
