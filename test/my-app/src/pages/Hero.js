0f5ebb5af1a357f10f55051cf37a9110ced0d0df
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-09-18 08:53:48 -0400

    assets

--- test/my-app/src/pages/Hero.js
+++ test/my-app/src/pages/Hero.js
@@ -5,9 +5,9 @@ import SurvivalImage from "../assets/survival.jpg"
 const Hero = () => (
   <div style={{ backgroundImage: `url(${SurvivalImage})`, backgroundSize: "cover", height: "75vh" }} >
   <div className="text-center hero my-5">
-    
-    
-      <h1 className="mb-4">Survivalism's Motto:</h1>
+
+
+      <h1 className="mb-4">Survivalist's Motto:</h1>
 
 
       <h2 className="lead display-4">
--- test/my-app/src/pages/Home.js
+++ test/my-app/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Content from "../pages/Content.js";
+import Hero from "../pages/Hero.js";
 
 
 const Home = () => {
   return (
     <div className="container">
-
+  <>
+    <Hero />
+    <hr />
+    <Content />
+  </>
     </div>
   );
 };
